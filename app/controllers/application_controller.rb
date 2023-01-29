66b14c7332efd6d661a0a5b93021c513e4491721
NRM129 <i.naru129@yahoo.ne.jp>
2023-01-29 23:40:15 +0900

    ログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,17 @@ class ApplicationController < ActionController::Base
 
   private
 
+  # Basic認証
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
+    # devise_parameter_sanitizer.permit(:sign_in,keys:[:email, :password])
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :email, length: { minimum: 6}
+    # validates :email
     VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
     validates :password, format: { with: VALID_PASSWORD_REGEX}
     validates :birthday
