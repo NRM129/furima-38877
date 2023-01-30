42bac49789df436e7f9d66fb4ab7f382ab957dbc
NRM129 <i.naru129@yahoo.ne.jp>
2023-01-30 20:32:15 +0900

    投稿機能の表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,15 +5,15 @@ class Item < ApplicationRecord
   # has_one :order
   
   with_options presence: true do
-    validates : image
-    validates : item_name
-    validates : explanation
-    validates : category_id
-    validates : condition_id
-    validates : postage_id
-    validates : prefecture_id
-    validates : preparation_id
-    validates : price
+    validates :image
+    validates :item_name
+    validates :explanation
+    validates :category_id
+    validates :condition_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :preparation_id
+    validates :price
   end
   
 end
