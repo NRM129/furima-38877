0c5be74cc237a80740caa8aea4eeca55b12700d9
NRM129 <i.naru129@yahoo.ne.jp>
2023-01-31 17:29:35 +0900

    画像アップロード機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,19 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new ]
+
+
   def index
     @item = Item.all
   end
 
   def new
     @item = Item.new
-    @item = Category.all
-    @item = Postage.all
-    @item = Condition.all
-    @item = Prefecture.all
-    @item = Preparation.all
   end
 
   def create
     @item = Item.new(item_params)
     if @item.save
+      binding.pry
       redirect_to root_path
     else
       render :new
@@ -24,6 +23,12 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:image, :item_name, :explanation, :category_id, :condition_id, :postage_id, :prefecture_id, :preparation_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
